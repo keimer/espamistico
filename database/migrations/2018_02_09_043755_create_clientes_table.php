d58b6cacb01386f9ea405c0ed651b178173e2f1d
Keimer <Keimerrzc@gmail.com>
2018-02-09 01:19:28 -0400

    Se solvento problema de ejecución del servidor al descargar el proyecto del repositorio, se realizaron varios pasos que fueron documentados, se creo la primera Migration y fue ejecutada con exito

---
+++ database/migrations/2018_02_09_043755_create_clientes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps(); 
+            $table->string('rut');
+            $table->string('pasaporte');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('telefono');
+            $table->string('celular');
+            $table->string('direccion');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+}
