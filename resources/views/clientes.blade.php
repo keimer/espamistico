795cb513f6e058dc9d9b142b3e6d2408bec4961e
GRP_SECURITY\kzamora <keimer.zamora@ext-security.cl>
2018-02-10 19:51:36 -0300

    Se adiciono un poco de Boostrap en la vista detalleCliente

--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="content">
         <!-- <div class="title m-b-md"> -->
-        <div>
+        <div class="text-left ">
             Clientes
         </div>
 
@@ -15,21 +15,33 @@
             <a href="https://forge.laravel.com">Forge</a>
             <a href="https://github.com/laravel/laravel">GitHub</a> -->
         </div>
+
+        <div class="progress">
+            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+        </div>
+
         <div class="row">
             <?php 
                 //dd($clientes);
                 // $clientes.lenght
+                //$clientes=[]
             ?>
-                        
             @forelse($clientes as $cliente)
-                <!-- <div class="col-6"> -->
-                    <a href='/detalleCliente/{{$cliente->id}}'>{{ $cliente->rut }} - </a>
-                    <p> {{ $cliente->nombre }} {{ $cliente->apellido }}</p>
-                    <!-- <p>{{-- $cliente->apellido --}}</p> -->
-                    <!-- <p>{{-- $cliente->created_at --}}</p> -->
-                <!-- </div> -->
+                <div class="col-8 text-left">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <p>
+                            <a href='/detalleCliente/{{$cliente->id}}'>{{ $cliente->rut }}</a> - {{ $cliente->nombre }} {{ $cliente->apellido }}
+                             </p>
+                            <!-- <p>{{-- $cliente->apellido --}}</p> -->
+                            <!-- <p>{{-- $cliente->created_at --}}</p> -->
+                        </li>
+                    </ul>
+                </div>
             @empty
-                <p>No existen clientes registrados en el sistema.</p>
+                <div class="alert alert-primary text-center" role="alert">
+                    <p class="text-center">No existen clientes registrados en el sistema.</p>
+                </div>
             @endforelse
             
             
