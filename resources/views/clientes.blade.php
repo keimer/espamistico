68bf8991e1e8046752a8940045e8f04c6a860ad6
GRP_SECURITY\kzamora <keimer.zamora@ext-security.cl>
2018-02-12 07:30:49 -0300

    Fue estilizado parte del diseño con Bootstrap usando div contenedores rows and col, para ello estudie platzi Bootstrap y diseño UX

--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
+@section('submenu')
+<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Crear</a>
+<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Modificar</a>
+<a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+<a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+</div>
+<div class="tab-content" id="v-pills-tabContent">
+<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">creando...</div>
+<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">Modificando...</div>
+<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">Otro...</div>
+<div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">Ultimo...</div>
+</div>
+@endsection
+
 @section('content')
     <div class="content">
         <!-- <div class="title m-b-md"> -->
@@ -8,23 +23,23 @@
         </div>
 
 
-        <div class="links">
+        <!-- <div class="links"> -->
             <!-- <a href="/">Home</a> -->
             <!-- <a href="https://laracasts.com">Laracasts</a>
             <a href="https://laravel-news.com">News</a>
             <a href="https://forge.laravel.com">Forge</a>
             <a href="https://github.com/laravel/laravel">GitHub</a> -->
-        </div>
+        <!-- </div> -->
 
-        <div class="progress">
+        <!-- <div class="progress">
             <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
-        </div>
+        </div> -->
 
         <div class="row">
             <?php 
                 //dd($clientes);
                 // $clientes.lenght
-                //$clientes=[]
+                $clientes=[]
             ?>
             @forelse($clientes as $cliente)
                 <div class="col-8 text-left">
@@ -33,8 +48,6 @@
                             <p>
                             <a href='/detalleCliente/{{$cliente->id}}'>{{ $cliente->rut }}</a> - {{ $cliente->nombre }} {{ $cliente->apellido }}
                              </p>
-                            <!-- <p>{{-- $cliente->apellido --}}</p> -->
-                            <!-- <p>{{-- $cliente->created_at --}}</p> -->
                         </li>
                     </ul>
                 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,69 +5,28 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Espacio Místico</title>
-
+        <!-- <title>Espacio Místico</title> -->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        
-        <!-- Fonts -->
-        <!-- <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <!-- <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            } 
-        </style> -->
+        <link rel="stylesheet" href="css/app.css">
+                
     </head>
     <body>
-        <div class="text-center">
+        <header>
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-sm-4 h2 no-gutters"> 
+                        Espacio Místico
+                    </div>
+                    <div class="col-sm-4">
+                    </div>
+                    <div class="col-sm-4 text-sm-right">
+                        <a href="#">Login</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div class="container">
             <!-- @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -78,49 +37,66 @@
                     @endauth
                 </div>
             @endif -->
-            <h1>Espacio Místico</h1>
-
-            <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
-            <!-- <a class="navbar-brand" href="#">Navbar w/ text</a> -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="/">Home </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/agenda">Agenda</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/clientes">Clientes</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/asesores">Asesores</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/profesionales">Profesionales</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/productos">Productos</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/expos">Expos</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/pagos">Pagos</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/inventario">Inventario</a>
-                </li>
-              </ul>
+            
+            <!-- **************************** -->
+            <!-- Barra de navegacion -->
+            <!-- **************************** -->
+            <div class="row no-gutters">
+                <div class="col-sm-12">
+                    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+                    <!-- <a class="navbar-brand" href="#">Navbar w/ text</a> -->
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                        <a class="nav-link" href="/">Home </a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/agenda">Agenda</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/clientes">Clientes</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/asesores">Asesores</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/profesionales">Profesionales</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/productos">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/expos">Expos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/pagos">Pagos</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/inventario">Inventario</a>
+                        </li>
+                    </ul>
+                    </div>
+                    </nav>
+                </div>
             </div>
-          </nav>
+            <!-- **************************** -->
 
-            <div class="container">
-                @yield('content')  
-            </div>
+            <!-- <div class="container"> -->
+                <div class="row no-gutters">
+                    <div class="col-sm-12 col-md-3 ">
+                        @yield('submenu')  
+                    </div>
+                    <div class="col-sm-1 ">  
+                    </div>
+                    <div class="col-sm-12 col-md-8">
+                        @yield('content')  
+                    </div>
+                </div>
+
+            <!-- </div> -->
         </div>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
